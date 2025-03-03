36f87413c664df54b42044a4fb5520e7cdb85367
kitUIN <kulujun@gmail.com>
2025-03-03 15:17:22 +0800

    Update | PluginLogo

--- ShadowViewer.Controls.PluginLogo/PluginLogo.xaml.cs
+++ ShadowViewer.Controls.PluginLogo/PluginLogo.xaml.cs
@@ -1,6 +1,7 @@
 using Microsoft.UI.Xaml.Controls;
 using System;
 using Microsoft.UI.Xaml;
+using System.Text.RegularExpressions;
 
 namespace ShadowViewer.Controls;
 
@@ -17,41 +18,60 @@ public sealed partial class PluginLogo : UserControl
         InitializeComponent();
     }
     /// <summary>
-    /// 
-    /// </summary>
-    public string Logo
+    /// 获取或设置Content的值
+    /// </summary>  
+    public string LogoSource
     {
+        get => (string)GetValue(LogoSourceProperty);
         set
         {
-            var url = new Uri(value);
-            switch (url.Scheme)
+            SetValue(LogoSourceProperty, value);
+            var uri = new Uri(value);
+            string glyph;
+            switch (uri.Scheme)
             {
                 case "ms-appx" or "http" or "https":
                     BitmapIcon.Visibility = Visibility.Visible;
-                    BitmapIcon.UriSource = url;
+                    BitmapIcon.UriSource = uri;
                     break;
                 case "font":
                     FontIcon.Visibility = Visibility.Visible;
-                    FontIcon.Glyph = value.Replace("font://", "");
+                    glyph = value.Replace("font://", "");
+                    if(glyph.StartsWith("\\")) glyph = Regex.Unescape(glyph);
+                    FontIcon.Glyph = glyph;
                     break;
                 case "fluent":
-                    switch (url.Host)
+                    switch (uri.Host)
                     {
                         case "regular":
                             FluentRegularIcon.Visibility = Visibility.Visible;
-                            FluentRegularIcon.Glyph = value.Replace("fluent://regular/", "");
+                            glyph = value.Replace("fluent://regular/", "");
+                            if (glyph.StartsWith("\\")) glyph = Regex.Unescape(glyph);
+                            FluentRegularIcon.Glyph = glyph;
                             break;
                         case "filled":
                             FluentFilledIcon.Visibility = Visibility.Visible;
-                            FluentFilledIcon.Glyph = value.Replace("fluent://filled/", "");
+                            glyph = value.Replace("fluent://filled/", "");
+                            if (glyph.StartsWith("\\")) glyph = Regex.Unescape(glyph);
+                            FluentFilledIcon.Glyph = glyph;
                             break;
                     }
-
                     break;
             }
         }
     }
 
+    /// <summary>
+    /// 标识 Content 依赖属性。
+    /// </summary>
+    public static readonly DependencyProperty LogoSourceProperty =
+        DependencyProperty.Register(nameof(LogoSource), typeof(string), typeof(PluginLogo),
+            new PropertyMetadata(null, OnLogoSourceChanged));
+
+    private static void OnLogoSourceChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+    {
+        
+    }
     /// <summary>
     /// 获取或设置Content的值
     /// </summary>  
@@ -66,12 +86,10 @@ public sealed partial class PluginLogo : UserControl
     /// </summary>
     public static readonly DependencyProperty FontIconSizeProperty =
         DependencyProperty.Register(nameof(FontIconSize), typeof(int), typeof(PluginLogo),
-            new PropertyMetadata(16, OnFontIconSizeChanged));
+            new PropertyMetadata(20, OnFontIconSizeChanged));
 
     private static void OnFontIconSizeChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
     {
-        var target = obj as PluginLogo;
-        if (target != null)  
-            target.FontIconSize = (int)args.NewValue;
+        
     }
 }
--- Test/MainWindow.xaml.cs
+++ Test/MainWindow.xaml.cs
@@ -22,46 +22,46 @@ namespace Test
            
         }
 
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-        {
-            TipContainerCenter.Notify("测试success",InfoBarSeverity.Success);
-        }
-
-        private void ButtonBase_OnClick2(object sender, RoutedEventArgs e)
-        {
-            TipContainerCenter.Notify("测试Info", InfoBarSeverity.Informational);
-        }
-        private void ButtonBase_OnClick3(object sender, RoutedEventArgs e)
-        {
-            TipContainerCenter.Notify("测试Warn长长的", InfoBarSeverity.Warning);
-        }
-        private void ButtonBase_OnClick4(object sender, RoutedEventArgs e)
-        {
-            TipContainerCenter.Notify("测试Error", InfoBarSeverity.Error);
-        }
-
-        private void ButtonBase_OnClick5(object sender, RoutedEventArgs e)
-        {
-            TipContainerCenter.Notify(new InfoBar
-            {
-                Message = "Test",
-                Severity = InfoBarSeverity.Error,
-                IsClosable = true,
-                IsIconVisible = true,
-                IsOpen = true,
-                Content = new Grid()
-                {
-                  Children  =
-                  {
-                      new ProgressBar()
-                      {
-                          IsIndeterminate = true
-                      }
-                  }
-                },
-                HorizontalAlignment = HorizontalAlignment.Center,
-                FlowDirection = FlowDirection.LeftToRight
-            }, displaySeconds: 0);
-        }
+        // private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        // {
+        //     TipContainerCenter.Notify("测试success",InfoBarSeverity.Success);
+        // }
+        //
+        // private void ButtonBase_OnClick2(object sender, RoutedEventArgs e)
+        // {
+        //     TipContainerCenter.Notify("测试Info", InfoBarSeverity.Informational);
+        // }
+        // private void ButtonBase_OnClick3(object sender, RoutedEventArgs e)
+        // {
+        //     TipContainerCenter.Notify("测试Warn长长的", InfoBarSeverity.Warning);
+        // }
+        // private void ButtonBase_OnClick4(object sender, RoutedEventArgs e)
+        // {
+        //     TipContainerCenter.Notify("测试Error", InfoBarSeverity.Error);
+        // }
+        //
+        // private void ButtonBase_OnClick5(object sender, RoutedEventArgs e)
+        // {
+        //     TipContainerCenter.Notify(new InfoBar
+        //     {
+        //         Message = "Test",
+        //         Severity = InfoBarSeverity.Error,
+        //         IsClosable = true,
+        //         IsIconVisible = true,
+        //         IsOpen = true,
+        //         Content = new Grid()
+        //         {
+        //           Children  =
+        //           {
+        //               new ProgressBar()
+        //               {
+        //                   IsIndeterminate = true
+        //               }
+        //           }
+        //         },
+        //         HorizontalAlignment = HorizontalAlignment.Center,
+        //         FlowDirection = FlowDirection.LeftToRight
+        //     }, displaySeconds: 0);
+        // }
     }
 }
